18279ed0b7cb970b2e5573f98ee9c4bca5ff7ff6
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-08-19 22:16:39 +0300

    Add audio system with music source

---
+++ Assets/Source/Scripts/Systems/Game/AudioSystem.cs
@@ -0,0 +1,10 @@
+﻿using Kuhpik;
+using UnityEngine;
+
+namespace GarbageScaler.Systems.Game
+{
+    public class AudioSystem : GameSystem
+    {
+        [SerializeField] private AudioSource musicSource;
+    }
+}
