de7c4561349145474e7368629f6af69ebdc2e00c
Empyupyu <nbi4ehb@gmail.com>
2024-08-20 10:39:11 +0400

    [Add] Audio sys

--- Assets/Source/Scripts/Systems/Game/AudioSystem.cs
+++ Assets/Source/Scripts/Systems/Game/AudioSystem.cs
@@ -1,4 +1,5 @@
-﻿using Kuhpik;
+﻿using System.Collections;
+using Kuhpik;
 using UnityEngine;
 
 namespace GarbageScaler.Systems.Game
@@ -6,5 +7,58 @@ namespace GarbageScaler.Systems.Game
     public class AudioSystem : GameSystem
     {
         [SerializeField] private AudioSource musicSource;
+        [SerializeField] private AudioClip _drive;
+        [SerializeField] private AudioClip _back;
+        [SerializeField] private AudioClip _idle;
+
+        private CarControl _carControl;
+        
+        public override void OnInit()
+        {
+            _carControl = FindObjectOfType<CarControl>();
+            _carControl.OnMove += DriveSound;
+            _carControl.OnMoveBack += BackDriveSound;
+            _carControl.OnIdle += IdleDriveSound;
+        }
+
+        private void IdleDriveSound()
+        {
+            if (musicSource.clip != null && musicSource.clip.Equals(_idle)) return;
+            
+            musicSource.clip = _idle;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+
+        private void BackDriveSound()
+        {
+            if (_isBackSoundPlay) return;
+            if (musicSource.clip != null && musicSource.clip.Equals(_back)) return;
+            
+            // musicSource.clip = _back;
+            // musicSource.loop = true;
+            // musicSource.Play();
+            StartCoroutine(BackSound());
+        }
+
+        private bool _isBackSoundPlay;
+        
+        private IEnumerator BackSound()
+        {
+            _isBackSoundPlay = true;
+            AudioSource.PlayClipAtPoint(_back, _carControl.transform.position);
+
+            yield return new WaitForSeconds(_back.length);
+            _isBackSoundPlay = false;
+        }
+
+        private void DriveSound()
+        {
+            if (musicSource.clip != null && musicSource.clip.Equals(_drive)) return;
+            
+            musicSource.clip = _drive;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
     }
 }
