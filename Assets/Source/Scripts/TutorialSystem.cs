7fd2cd14cf8e8573e32b2b52940d0362a73484c0
Empyupyu <nbi4ehb@gmail.com>
2024-08-20 18:18:51 +0400

    [Add] Tutor

--- Assets/Source/Scripts/TutorialSystem.cs
+++ Assets/Source/Scripts/TutorialSystem.cs
@@ -11,12 +11,27 @@ namespace GarbageScaler
         private CarControl _carControl;
         private CraneController _craneController;
 
-        public CinemachineDollyCart CinemachineDollyCart;
+        public GameObject Arrow1, Arrow2, Arrow3, Arrow4;
+
+        public CinemachineVirtualCamera C1;
+        public CinemachineVirtualCamera C2;
+        public CinemachineVirtualCamera C3;
+        public CinemachineVirtualCamera C4;
         
         public override void OnInit()
         {
             _carControl = FindObjectOfType<CarControl>();
             _craneController = FindObjectOfType<CraneController>();
+
+            C2?.gameObject.SetActive(false);
+            C3?.gameObject.SetActive(false);
+            C4?.gameObject.SetActive(false);
+            Arrow1?.SetActive(false);
+            Arrow2?.SetActive(false);
+            Arrow3?.SetActive(false);
+            // Arrow4?.SetActive(false);
+
+            StartCoroutine(TutorialLoop());
         }
 
         private bool _playerCompletedFirstStep;
@@ -26,8 +41,26 @@ namespace GarbageScaler
         {
             //Включаю камеру летящую на игрока
             //отключаем у игрока все кнопки
+            _carControl.enabled = false;
+            _craneController.enabled = false;
             
-            yield return new WaitForSeconds(1);
+            C1.gameObject.SetActive(false);
+            C2.gameObject.SetActive(true);
+          
+            
+            yield return new WaitForSeconds(2.9f);
+
+            C2.gameObject.SetActive(false);
+            C3.gameObject.SetActive(true);
+            
+            yield return new WaitForSeconds(2);
+            
+            _carControl.enabled = true;
+            _craneController.enabled = true;
+            
+            yield return new WaitForSeconds(.5f);
+            
+            Arrow1.SetActive(true);
             //Камера игрока
             //включаем у игрока все кнопки
             //Включаю стрелку и камеру на стрелку
