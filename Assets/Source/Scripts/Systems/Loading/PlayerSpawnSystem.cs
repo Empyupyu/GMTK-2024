ef8ae6844b0c9fa2a7c9b44340373b5ba498223d
Nikita Smolianitski <n.smolianitski@gmail.com>
2024-08-20 15:59:16 +0300

    Add shop environment

--- Assets/Source/Scripts/Systems/Loading/PlayerSpawnSystem.cs
+++ Assets/Source/Scripts/Systems/Loading/PlayerSpawnSystem.cs
@@ -1,12 +1,15 @@
 ﻿using Kuhpik;
+using UnityEngine;
 
 namespace GarbageScaler.Systems.Loading
 {
     public class PlayerSpawnSystem : GameSystem
     {
+        [SerializeField] private Transform playerSpawnPoint;
+        
         public override void OnInit()
         {
-            game.Player = Instantiate(config.PlayerPrefab);
+            game.Player = Instantiate(config.PlayerPrefab, playerSpawnPoint.position, Quaternion.identity);
         }
     }
 }
